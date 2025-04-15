ff760dffea22db837f29b701c1fbd9b187762872
Mogan-ram <moganram10@gmail.com>
2025-04-15 18:41:25 +0530

    fixing_fix

--- front-end/src/components/mentorship/MentorshipRequest.js
+++ front-end/src/components/mentorship/MentorshipRequest.js
@@ -7,8 +7,6 @@ import LoadingSpinner from '../common/LoadingSpinner';
 import { toast } from 'react-toastify';
 import { FaEnvelope } from 'react-icons/fa';
 
-
-
 const MentorshipRequest = () => {
     const [projects, setProjects] = useState([]);
     const [selectedProject, setSelectedProject] = useState('');
